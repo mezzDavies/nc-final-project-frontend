5e45f7218fb25dbe5a897413aad1578f953261c5
Ewan Childs <ewan.childs@gmail.com>
2022-03-21 15:40:55 +0000

    added navigation for footer and homepage

---
+++ components/Household.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Household = () => {
+  return (
+    <View>
+      <Text>This is the Household !!!!</Text>
+    </View>
+  );
+};
+
+export default Household;
