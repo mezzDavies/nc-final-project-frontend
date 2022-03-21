7427c688ee0b7d36d2e16a0d17d9fc87866b798b
tantan628 <holttanis@gmail.com>
2022-03-21 18:00:10 +0000

    create FormTextField

---
+++ components/FormTextField.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, TextInput } from 'react-native';
+
+export const FormTextField = (props) => {
+    return (
+        <View>
+            <TextInput {...props} />
+            {props.errorText && (
+                <Text>{props.errorText}</Text>
+            )}
+        </View>
+    );
+}
