2008ad690105a3482c6fb2db6e990d6c3ec6ffcb
tantan628 <holttanis@gmail.com>
2022-03-23 18:22:09 +0000

    add rendering logic to HomePage based on if user logged in

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -1,30 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button, View, Text } from "react-native";
+import { auth } from "../firebase";
+import { fireDB } from "../firebase";
+import { doc, getDoc } from "firebase/firestore";
 
 const Homepage = ({ navigation }) => {
-  return (
-    <>
+  const [userStatus, setUserStatus] = useState(false);
+  const [firstName, setFirstName] = useState('');
+
+  useEffect(() => {
+    auth.onAuthStateChanged(function(user) {
+      if(user) {
+        setUserStatus(true);
+        auth.currentUser.getIdTokenResult()
+        .then(({ claims }) => {
+          const userDocRef = doc(fireDB, 'users', claims.user_id);
+          return getDoc(userDocRef)
+        })
+        .then((docSnap) => {
+          if(docSnap.exists()) {
+            const userData = docSnap.data();
+            setFirstName(userData.name);
+          }
+        })
+      }
+    })
+  }, [])
+
+  if(userStatus) {
+    return (
       <View>
-        <Text style={{ textAlign: "center", marginTop: 300 }}>Home Screen</Text>
+        <Text style={{ textAlign: "center", marginTop: 300 }}>Hello {firstName}, welcome back!</Text>
         <Button
           title="Go to recipe..."
           onPress={() => navigation.navigate("RecipePage")}
         />
         <Button
-          title="Go to sign up..."
-          onPress={() => navigation.navigate("SignUp")}
-        />
-        <Button
-          title="Go to sign in..."
-          onPress={() => navigation.navigate("SignIn")}
+          title="Go to profile..."
+          onPress={() => navigation.navigate("Profile")}
         />
         <Button
           title="Go to search page in..."
           onPress={() => navigation.navigate("SearchPage")}
         />
       </View>
-    </>
-  );
+    );
+  } else {
+    return (
+      <>
+        <View>
+          <Text style={{ textAlign: "center", marginTop: 300 }}>Home Screen</Text>
+          <Button
+            title="Go to recipe..."
+            onPress={() => navigation.navigate("RecipePage")}
+          />
+          <Button
+            title="Go to sign up..."
+            onPress={() => navigation.navigate("SignUp")}
+          />
+          <Button
+            title="Go to sign in..."
+            onPress={() => navigation.navigate("SignIn")}
+          />
+          <Button
+            title="Go to search page in..."
+            onPress={() => navigation.navigate("SearchPage")}
+          />
+        </View>
+      </>
+    );
+  }
 };
 
 export default Homepage;
