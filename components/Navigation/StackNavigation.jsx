e49c97b6b624f71e5684c312facf533884e02146
joshua-walters <87895876+jothwas@users.noreply.github.com>
2022-03-23 16:35:04 +0000

    add recipe list, recipe card, search function

--- components/Navigation/StackNavigation.jsx
+++ components/Navigation/StackNavigation.jsx
@@ -11,6 +11,7 @@ import MealPlanScreen from "../MealPlan";
 import SignUpPage from "../auth-components/SignUpPage";
 import SignInPage from "../auth-components/SignInPage";
 import TestingPage from "./Testing";
+import SearchPage from "../SearchPage";
 
 const Stack = createStackNavigator();
 
@@ -24,6 +25,7 @@ const HomepageNavigation = () => {
         <Stack.Screen name="SignIn" component={SignInPage} />
         <Stack.Screen name="Profile" component={ProfileScreen} />
         <Stack.Screen name="Testing" component={TestingPage} />
+        <Stack.Screen name="SearchPage" component={SearchPage} />
       </Stack.Navigator>
     </>
   );
