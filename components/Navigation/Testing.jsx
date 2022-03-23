7d51774c730237a7cb58a8d1ef8caacae2ceeee2
Ewan Childs <ewan.childs@gmail.com>
2022-03-23 14:44:04 +0000

    testing component for rob

---
+++ components/Navigation/Testing.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text} from "react-native";
+
+const TestingPage = () => {
+  return (
+    <View>
+      <Text style={{ textAlign: "center", marginTop: 300 }}>Welcome to testing!!!</Text>
+    </View>
+  );
+};
+
+export default TestingPage;
