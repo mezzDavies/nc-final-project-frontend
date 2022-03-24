2d214cc69dd6e3f461b737b8d59bff9101c7230d
mezzDavies <93786981+mezzDavies@users.noreply.github.com>
2022-03-24 11:55:55 +0000

    display (unstyled) hard-coded recipe data using getRecipeById function

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { Button } from "react-native-web";
+import { Button } from "react-native";
 
 const Homepage = ({ navigation }) => {
   return (
