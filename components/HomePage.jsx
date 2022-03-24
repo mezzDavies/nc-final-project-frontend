744a256cc0253e9bac398e7c36194c96cf8df2ca
Ewan Childs <ewan.childs@gmail.com>
2022-03-24 12:43:39 +0000

    finished else statement

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -7,72 +7,84 @@ import RandomRecipes from "./RandomRecipes";
 
 const Homepage = ({ navigation }) => {
   const [userStatus, setUserStatus] = useState(false);
-  const [firstName, setFirstName] = useState('');
+  const [firstName, setFirstName] = useState("");
 
   useEffect(() => {
-    auth.onAuthStateChanged(function(user) {
-      if(user) {
+    auth.onAuthStateChanged(function (user) {
+      if (user) {
         setUserStatus(true);
-        auth.currentUser.getIdTokenResult()
-        .then(({ claims }) => {
-          const userDocRef = doc(fireDB, 'users', claims.user_id);
-          return getDoc(userDocRef)
-        })
-        .then((docSnap) => {
-          if(docSnap.exists()) {
-            const userData = docSnap.data();
-            setFirstName(userData.name);
-          }
-        })
+        auth.currentUser
+          .getIdTokenResult()
+          .then(({ claims }) => {
+            const userDocRef = doc(fireDB, "users", claims.user_id);
+            return getDoc(userDocRef);
+          })
+          .then((docSnap) => {
+            if (docSnap.exists()) {
+              const userData = docSnap.data();
+              setFirstName(userData.name);
+            }
+          });
       } else {
         setUserStatus(false);
-        setFirstName('');
+        setFirstName("");
       }
-    })
-  }, [userStatus])
-  
-  if(userStatus) {
+    });
+  }, [userStatus]);
+
+  if (userStatus) {
     return (
       <ScrollView>
-      <View>
-      <RandomRecipes navigation={navigation} />
-        <Button
-          title="Go to Testing..."
-          onPress={() => navigation.navigate("Testing")}
-        />
-        <Button
-          title="Go to profile..."
-          onPress={() => navigation.navigate("Account", { screen: "Profile" })}
-        />
-        <Button
-          title="Go to search page in..."
-          onPress={() => navigation.navigate("SearchPage")}
-        />
-      </View>
-    </ScrollView>
-    );
-  } else {
-    return (
-      <>
         <View>
-          <Text style={{ textAlign: "center", marginTop: 300 }}>Home Screen</Text>
+          <RandomRecipes navigation={navigation} />
           <Button
-            title="Go to recipes..."
-            onPress={() => navigation.navigate("RecipePage")}
+            title="Go to Testing..."
+            onPress={() => navigation.navigate("Testing")}
           />
           <Button
-            title="Go to sign up..."
-            onPress={() => navigation.navigate("Account", { screen: "SignUp" })}
-          />
-          <Button
-            title="Go to sign in..."
-            onPress={() => navigation.navigate("Account", { screen: "SignIn" })}
+            title="Go to profile..."
+            onPress={() =>
+              navigation.navigate("Account", { screen: "Profile" })
+            }
           />
           <Button
             title="Go to search page in..."
             onPress={() => navigation.navigate("SearchPage")}
           />
         </View>
+      </ScrollView>
+    );
+  } else {
+    return (
+      <>
+        <ScrollView>
+          <View>
+            <RandomRecipes navigation={navigation} />
+            <Text style={{ textAlign: "center", marginTop: 300 }}>
+              Home Screen
+            </Text>
+            <Button
+              title="Go to recipes..."
+              onPress={() => navigation.navigate("RecipePage")}
+            />
+            <Button
+              title="Go to sign up..."
+              onPress={() =>
+                navigation.navigate("Account", { screen: "SignUp" })
+              }
+            />
+            <Button
+              title="Go to sign in..."
+              onPress={() =>
+                navigation.navigate("Account", { screen: "SignIn" })
+              }
+            />
+            <Button
+              title="Go to search page in..."
+              onPress={() => navigation.navigate("SearchPage")}
+            />
+          </View>
+        </ScrollView>
       </>
     );
   }
