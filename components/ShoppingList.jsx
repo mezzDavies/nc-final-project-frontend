418048da80347b19315a0ad1830291100929e825
mezzDavies <93786981+mezzDavies@users.noreply.github.com>
2022-03-28 09:32:33 +0100

    basic (hard-coded) functionality to shopping list page

--- components/ShoppingList.jsx
+++ components/ShoppingList.jsx
@@ -1,13 +1,35 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, ScrollView, Button } from "react-native";
+import { calculateShoppingList } from "../api/firestoreFunctions.mealPlans";
 
 const ShoppingListScreen = () => {
+  const [recipeIds, setRecipeIds] = useState([652134, 511728, 648432]);
+  const [shoppingList, setShoppingList] = useState([]);
+
+  useEffect(() => {
+    calculateShoppingList(recipeIds).then((res) => {
+      console.log(res);
+      setShoppingList(res);
+    });
+  }, []);
+
   return (
-    <View>
-      <Text style={{ textAlign: "center", marginTop: 300 }}>
-        ShoppingList screen
-      </Text>
-    </View>
+    <ScrollView>
+      <View>
+        <Text style={{ textAlign: "center", marginTop: 15 }}>
+          Shopping List
+        </Text>
+      </View>
+      <View>
+        {shoppingList.map((listItem) => {
+          return (
+            <Text
+              key={listItem.id}
+            >{`${listItem.name} - ${listItem.amount} ${listItem.unit}`}</Text>
+          );
+        })}
+      </View>
+    </ScrollView>
   );
 };
 
