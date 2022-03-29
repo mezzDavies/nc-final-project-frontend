3bb80cc181428a74326da0b9a1a6493abc416d5b
Ewan Childs <ewan.childs@gmail.com>
2022-03-29 11:54:50 +0100

    meal plan shizz

--- components/Household-components/JoinGroupScreen.jsx
+++ components/Household-components/JoinGroupScreen.jsx
@@ -1,6 +1,6 @@
 //IMPORTS - react
 import React, { useState, useEffect } from "react";
-import { View, Button, Text } from 'react-native';
+import { View, Button, Text } from "react-native";
 import { useForm, Controller } from "react-hook-form";
 import { FormTextField } from "../FormTextField";
 
@@ -8,73 +8,79 @@ import { FormTextField } from "../FormTextField";
 import { auth, fireDB } from "../../firebase";
 import { doc, getDoc } from "firebase/firestore";
 
-
 //IMPORTS - utils functions
 import getUserDataAndClaims from "../../utils/getUserDataAndClaims";
 import { addUserToFamily } from "../../api/firestoreFunctions.families";
 
-
 //----------COMPONENT----------
 const JoinGroupScreen = ({ navigation }) => {
-    //-----Declarations-----
-    const [loadingMessage, setLoadingMessage] = useState('');
-    const [userId, setUserId] = useState('');
-    const [parentStatus, setParentStatus] = useState(false);
-    const [firstName, setFirstName] = useState('');  
-    const { control, handleSubmit, reset, formState: { errors } } = useForm();
+  //-----Declarations-----
+  const [loadingMessage, setLoadingMessage] = useState("");
+  const [userId, setUserId] = useState("");
+  const [parentStatus, setParentStatus] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
-    const onSubmit = (data) => {
-        const inviteCode = data.inviteCode;
+  const onSubmit = (data) => {
+    const inviteCode = data.inviteCode;
 
-        setLoadingMessage(`We're just loading up the group now...`);
-        addUserToFamily(userId, inviteCode)
-            .then(() => {
-                reset();
-                navigation.navigate("Account", { screen: "Household" });
-            })
-    }
+    setLoadingMessage(`We're just loading up the group now...`);
+    addUserToFamily(userId, inviteCode).then(() => {
+      reset();
+      navigation.navigate("Account", { screen: "Household" });
+    });
+  };
 
-    //-----Use Effects-----
-    useEffect(() => {
-        getUserDataAndClaims()
-            .then(({ claims, userData, userId }) => {
-                setUserId(claims.user_id);
-                setFirstName(userData.name);
-                if(claims.parent) {
-                    setParentStatus(true);
-                }
-            })
-    }, []);
+  //-----Use Effects-----
+  useEffect(() => {
+    getUserDataAndClaims().then(({ claims, userData, userId }) => {
+      setUserId(claims.user_id);
+      setFirstName(userData.name);
+      if (claims.parent) {
+        setParentStatus(true);
+      }
+    });
+  }, []);
 
-    //-----Rendering-----
-    return (
-        <View>
-            <Text>Hi {firstName}, it looks like you're trying to join an existing group, to do this you'll need an invite code for that group. If you don't have one, ask a member of the group to send you one and you can join immediately!</Text>
-            <Controller
-                defaultValue=""
-                control={control}
-                rules={{
-                required: {
-                    value: true,
-                    message: "Please enter your invite code above.",
-                },
-                }}
-                render={({ field: { onChange, value } }) => (
-                <FormTextField
-                    error={errors.inviteCode}
-                    errorText={errors.inviteCode?.message}
-                    placeholder="Invite Code"
-                    onChangeText={(value) => onChange(value)}
-                    value={value}
-                />
-                )}
-                name="inviteCode"
-            />
-            <Button title="Submit" onPress={handleSubmit(onSubmit)} />
-            <Text>{loadingMessage}</Text>
-        </View>
-    )
-}
+  //-----Rendering-----
+  return (
+    <View>
+      <Text>
+        Hi {firstName}, it looks like you're trying to join an existing group,
+        to do this you'll need an invite code for that group. If you don't have
+        one, ask a member of the group to send you one and you can join
+        immediately!
+      </Text>
+      <Controller
+        defaultValue=""
+        control={control}
+        rules={{
+          required: {
+            value: true,
+            message: "Please enter your invite code above.",
+          },
+        }}
+        render={({ field: { onChange, value } }) => (
+          <FormTextField
+            error={errors.inviteCode}
+            errorText={errors.inviteCode?.message}
+            placeholder="Invite Code"
+            onChangeText={(value) => onChange(value)}
+            value={value}
+          />
+        )}
+        name="inviteCode"
+      />
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+      <Text>{loadingMessage}</Text>
+    </View>
+  );
+};
 
 //-----EXPORTS-----
 export default JoinGroupScreen;
--- components/MealPlan/components/MealPlanList.jsx
+++ components/MealPlan/components/MealPlanList.jsx
@@ -4,53 +4,132 @@ import {
   getMealPlan,
   toggleMealPlanStatus,
 } from "../../../api/firestoreFunctions.mealPlans";
-import { getRecipeById } from "../../../api/firestoreFunctions.recipes";
-import getUserDataAndClaims from "../../../utils/getUserDataAndClaims";
 import MealPlanCard from "./MealPlanCard";
+import { getMealPlans } from "../../../api/firestoreFunctions.mealPlans";
+import { getRecipeCardById } from "../../../api/firestoreFunctions.recipes";
+import {
+  getSelectionLists,
+  getSelectionList,
+} from "../../../api/firestoreFunctions.selectionLists";
+import getUserDataAndClaims from "../../../utils/getUserDataAndClaims";
+import { getFamilies } from "../../../api/firestoreFunctions.families";
 
 const MealPlanList = ({ navigation }) => {
-  const familyId = "yPRj8Q1cEgwJ465bec04";
-  const selectionListId = "oeAuz0njIbYyPeLUqpUw";
-  const mealPlanId = "V00CM4zYfykUNjxUXI80";
-
   const [mealPlan, setMealPlan] = useState([]);
   const [mealPlanConfirmation, setMealPlanConfirmation] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [isChild, setIsChild] = useState(false);
+  const [familyId, setFamilyId] = useState("");
+  const [selectionListId, setSelectionListId] = useState("");
+  const [selectionList, setSelectionList] = useState("");
+  const [userId, setUserId] = useState("");
+  const [mealPlanId, setMealPlanId] = useState([]);
+  const [recipeCards, setRecipeCards] = useState([]);
 
   useEffect(() => {
-    getMealPlan(familyId, selectionListId, mealPlanId)
-      .then(({ isConfirmed, recipeIds }) => {
-        setMealPlanConfirmation(isConfirmed);
-        return recipeIds;
-      })
-      .then((recipeIds) => {
-        const recipes = recipeIds.map((id) => {
-          return getRecipeById(id);
-        });
-        return Promise.all(recipes);
-      })
-      .then((recipes) => {
-        return recipes.map((recipe) => {
-          const { summary } = recipe;
-          return summary;
-        });
-      })
-      .then((recipes) => {
-        setMealPlan(recipes);
-        setIsLoading(false);
-      })
-      .catch((err) => console.log(err));
-  }, []);
-
-  useEffect(() => {
+    setIsLoading(true);
     getUserDataAndClaims()
-      .then(({ claims, useData }) => {
-        if (claims.parent) setIsChild(false);
-        else setIsChild(true);
+      .then(({ claims, userData, newUserId }) => {
+        setUserId(newUserId);
+        return newUserId;
       })
-      .catch((err) => console.log(err));
+      .then((userId) => {
+        return getFamilies(userId)
+          .then((familyId) => {
+            setFamilyId(familyId[0]);
+            return familyId[0];
+          })
+          .then((res) => {
+            getSelectionLists(res)
+              .then((selectionId) => {
+                setSelectionListId(selectionId[0]);
+                const newFamilyId = res;
+                const newSelectionListId = selectionId[0];
+                return Promise.all([newFamilyId, newSelectionListId]);
+              })
+              .then(([newFamilyId, newSelectionListId]) => {
+                return Promise.all([
+                  getSelectionList(newFamilyId, newSelectionListId),
+                  newFamilyId,
+                  newSelectionListId,
+                ]);
+              })
+              .then(([finalSelectionList, newFamilyId, newSelectionListId]) => {
+                setSelectionList(finalSelectionList[0]);
+                const mealPlanId = getMealPlans(
+                  newFamilyId,
+                  newSelectionListId
+                );
+                return Promise.all([
+                  newFamilyId,
+                  newSelectionListId,
+                  mealPlanId,
+                ]);
+              })
+              .then(([newFamilyId, newSelectionListId, mealPlanIdentity]) => {
+                setMealPlanId(mealPlanIdentity[0].mealPlanId);
+                return getMealPlan(
+                  newFamilyId,
+                  newSelectionListId,
+                  mealPlanIdentity[0].mealPlanId
+                );
+              })
+              .then((mealPlan) => {
+                setMealPlan(mealPlan.recipeIds);
+                return Promise.all([
+                  mealPlan.recipeIds.map((recipe) => getRecipeCardById(recipe)),
+                ]);
+              })
+              .then(([recipeCards]) => {
+                return Promise.all(recipeCards);
+              })
+              .then((result) => {
+                setRecipeCards(result);
+                setIsLoading(false);
+              });
+          });
+      });
   }, []);
+
+  // useEffect(() => {
+  //   getRecipeCardsById(mealPlan).then((res) => {
+  //     console.log(res);
+  //   });
+  // }, [mealPlan]);
+
+  // useEffect(() => {
+  //   getMealPlan(familyId, selectionListId, mealPlanId)
+  //     .then(({ isConfirmed, recipeIds }) => {
+  //       setMealPlanConfirmation(isConfirmed);
+  //       return recipeIds;
+  //     })
+  //     .then((recipeIds) => {
+  //       const recipes = recipeIds.map((id) => {
+  //         return getRecipeById(id);
+  //       });
+  //       return Promise.all(recipes);
+  //     })
+  //     .then((recipes) => {
+  //       return recipes.map((recipe) => {
+  //         const { summary } = recipe;
+  //         return summary;
+  //       });
+  //     })
+  //     .then((recipes) => {
+  //       setMealPlan(recipes);
+  //       setIsLoading(false);
+  //     })
+  //     .catch((err) => console.log(err));
+  // }, []);
+
+  // useEffect(() => {
+  //   getUserDataAndClaims()
+  //     .then(({ claims, useData }) => {
+  //       if (claims.parent) setIsChild(false);
+  //       else setIsChild(true);
+  //     })
+  //     .catch((err) => console.log(err));
+  // }, []);
 
   if (isLoading) return <Text>Is Loading...</Text>;
 
@@ -58,7 +137,7 @@ const MealPlanList = ({ navigation }) => {
     <View>
       <Text>
         Meal Plan contents:
-        {mealPlan.map((recipe, index) => {
+        {recipeCards.map((recipe, index) => {
           return (
             <MealPlanCard
               recipe={recipe}
@@ -68,7 +147,7 @@ const MealPlanList = ({ navigation }) => {
           );
         })}
       </Text>
-      <Button
+      {/* <Button
         title={
           isChild
             ? "Only adult user can confirm Meal Plans"
@@ -83,7 +162,7 @@ const MealPlanList = ({ navigation }) => {
           });
         }}
         disabled={isChild ? true : mealPlanConfirmation}
-      />
+      /> */}
     </View>
   );
 };
