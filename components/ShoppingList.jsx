80845dc65154dac5ff750fafdd55ec01e12ab41b
joshua-walters <87895876+jothwas@users.noreply.github.com>
2022-03-30 16:46:16 +0100

    amend styling on list page

--- components/ShoppingList.jsx
+++ components/ShoppingList.jsx
@@ -6,6 +6,7 @@ import getUserDataAndClaims from "../utils/getUserDataAndClaims";
 import ShoppingListCard from "./ShoppingListCard";
 import { auth } from "../firebase";
 import UserNotLoggedIn from "./UserNotLoggedIn";
+import styles from "./ShoppingStyles";
 
 const ShoppingListScreen = () => {
   const [userStatus, setUserStatus] = useState(false);
@@ -16,66 +17,83 @@ const ShoppingListScreen = () => {
   useEffect(() => {
     setIsLoading(true);
     auth.onAuthStateChanged(function (user) {
-      if(user) {
+      if (user) {
         setUserStatus(true);
         getUserDataAndClaims()
-        .then(({ claims, userData, newUserId }) => {
-          if(!userData.groupIds?.length > 0) {
-            setFamilyStatus(false)
-            setIsLoading(false)
-            return Promise.reject({ status: 400, message: "Not a member of any group"})
-          } else {
-            setFamilyStatus(true)
-            const currentIds = {
-              user: newUserId,
-              family: userData.groupIds[0]
+          .then(({ claims, userData, newUserId }) => {
+            if (!userData.groupIds?.length > 0) {
+              setFamilyStatus(false);
+              setIsLoading(false);
+              return Promise.reject({
+                status: 400,
+                message: "Not a member of any group",
+              });
+            } else {
+              setFamilyStatus(true);
+              const currentIds = {
+                user: newUserId,
+                family: userData.groupIds[0],
+              };
+              return Promise.all([
+                currentIds,
+                getSelectionLists(currentIds.family),
+              ]);
             }
-            return Promise.all([currentIds, getSelectionLists(currentIds.family)])
-          }
-        })
-        .then(([currentIds, newSelectionId]) => {
-          currentIds.selection = newSelectionId[0]
-          return Promise.all([currentIds, getMealPlans(currentIds.family, currentIds.selection)]);
-        })
-        .then(([currentIds, newMealPlanId]) => {
-          currentIds.mealPlan = newMealPlanId[0].mealPlanId
-          return getMealPlan(currentIds.family, currentIds.selection, currentIds.mealPlan);
-        })
-        .then((mealPlan) => {
-          setShoppingList(mealPlan.shoppingList);
-          setIsLoading(false);
-        })
-        .catch((err) => {
-          return err
-        });
+          })
+          .then(([currentIds, newSelectionId]) => {
+            currentIds.selection = newSelectionId[0];
+            return Promise.all([
+              currentIds,
+              getMealPlans(currentIds.family, currentIds.selection),
+            ]);
+          })
+          .then(([currentIds, newMealPlanId]) => {
+            currentIds.mealPlan = newMealPlanId[0].mealPlanId;
+            return getMealPlan(
+              currentIds.family,
+              currentIds.selection,
+              currentIds.mealPlan
+            );
+          })
+          .then((mealPlan) => {
+            setShoppingList(mealPlan.shoppingList);
+            setIsLoading(false);
+          })
+          .catch((err) => {
+            return err;
+          });
       } else {
         setUserStatus(false);
         setShoppingList([]);
         setIsLoading(false);
       }
-    })
+    });
   }, [userStatus]);
 
   if (isLoading) return <Text>Loading...</Text>;
-  if (!userStatus) return <UserNotLoggedIn setUserStatus={setUserStatus} />
-  if (!familyStatus) return <Text>It looks like you're not part of a group yet, take a look at the household page under your account.</Text>
+  if (!userStatus) return <UserNotLoggedIn setUserStatus={setUserStatus} />;
+  if (!familyStatus)
+    return (
+      <Text>
+        It looks like you're not part of a group yet, take a look at the
+        household page under your account.
+      </Text>
+    );
 
   return (
     <ScrollView>
-      <View>
-        <Text style={{ textAlign: "center", marginTop: 15, marginBottom: 10 }}>
-          This Week's Shopping List:
-        </Text>
-      </View>
-      <View>
-        {shoppingList.map((listItem, index) => {
-          return (
-            <ShoppingListCard
-              listItem={listItem}
-              key={`${listItem.id} + ${index}`}
-            />
-          );
-        })}
+      <View style={styles.list}>
+        <View style={styles.container}>
+          <Text style={styles.title}>This Week's Shopping List:</Text>
+          {shoppingList.map((listItem, index) => {
+            return (
+              <ShoppingListCard
+                listItem={listItem}
+                key={`${listItem.id} + ${index}`}
+              />
+            );
+          })}
+        </View>
       </View>
     </ScrollView>
   );
